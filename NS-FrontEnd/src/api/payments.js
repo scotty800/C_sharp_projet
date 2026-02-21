06c7b6b700668460cc2afbc12aae4c4877f166e8
scotty <scottyndanga02400@gmail.com>
2026-02-21 16:47:08 +0100

    Supprime ancien frontend React pour migration Next.js

--- NS-FrontEnd/src/api/payments.js
+++
@@ -1,8 +0,0 @@
-import api from './config';
-
-export const paymentsApi = {
-  createPaymentIntent: (data) => api.post('/payments/create-intent', data),
-  confirmPayment: (data) => api.post('/payments/confirm', data),
-  refundPayment: (orderId, data) => api.post(`/payments/${orderId}/refund`, data),
-  getPaymentIntent: (paymentIntentId) => api.get(`/payments/intent/${paymentIntentId}`)
-};
--- NS-FrontEnd/src/api/users.js
+++
@@ -1,9 +0,0 @@
-import api from './config';
-
-export const usersApi = {
-  getProfile: () => api.get('/users/me'),
-  updateProfile: (data) => api.put('/users/me', data),
-  updatePassword: (data) => api.put('/users/me/password', data),
-  getOrders: () => api.get('/users/me/orders'),
-  getReviews: () => api.get('/users/me/reviews')
-};
